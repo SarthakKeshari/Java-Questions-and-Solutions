f40f49056a8262255e6d01cfe724caa7c0eabf95
Vedansh-Keshari <vedanshkeshari14@gmail.com>
2021-09-28 13:32:18 +0530

    Added solution for positive and negative number

---
+++ Basic Codes/Positive_Negative.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Positive_Negative {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter any number");
+        int n=sc.nextInt();
+        if(n>=0){
+            System.out.println("Positive");
+        }
+        else {
+            System.out.println("Negative");
+
+        }
+    }
+}
