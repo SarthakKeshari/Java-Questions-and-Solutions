d9efec81de6c90447a5b48c27306b328c098f575
Dinesh Yogesh Parekh <dinesh21045@gmail.com>
2021-09-28 21:00:23 +0530

    Solution greater than 100

---
+++ Basic Codes/Greaterno.java
@@ -0,0 +1,18 @@
+
+public class Greaterno {
+	
+	public static void main (String[] args) {
+		
+		int num=110;
+		System.out.println(num);
+		if(num>100)
+		{
+			System.out.println("Number is Greater than 100");
+		}
+		else {
+			System.out.println("Number is Less than 100");
+		}
+		
+	}
+
+}
