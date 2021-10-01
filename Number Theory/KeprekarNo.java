34ab99fcddbe3fcfe79708d044cd3d0e3a2d65b6
Vedansh-Keshari <vedanshkeshari14@gmail.com>
2021-10-01 13:53:11 +0530

    Solution for Keprekar Number

---
+++ Number Theory/KeprekarNo.java
@@ -0,0 +1,20 @@
+//Write a program to check if a number is Special or not
+import java.util.*;
+class Keprekar{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter any number");   //Input number from the user
+        int n=sc.nextInt();
+
+        String s=String.valueOf(n);
+        int l=s.length();
+        int sq=n*n;
+        int sum=(sq/(int)(Math.pow(10,l)))+(sq%(int)(Math.pow(10,l)));    // Find the sum of the two divided parts of the number
+        if(sum==n){                                                       // Compare whether the sum is equal to the number or not
+            System.out.println(n+" is a Keprekar Number");
+        }
+        else{
+            System.out.println(n+" is not a Keprekar Number");
+        }
+    }
+}
