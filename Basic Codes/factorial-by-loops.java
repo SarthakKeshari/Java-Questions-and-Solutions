0a18b33bc5cb77c51e884ce8494f2c734cb907a8
Vishal Chauhan <44545745+vish-han@users.noreply.github.com>
2021-10-01 10:04:50 +0530

    Update factorial-by-loops.java

--- Basic Codes/factorial-by-loops.java
+++ Basic Codes/factorial-by-loops.java
@@ -1,3 +1,6 @@
+//Question: You have to make an algorithm to find factorial of a given number;
+
+//factorial of n is n*(n-1)*.....*2*1;
 package com.company;
 
 import java.util.Scanner;
@@ -9,9 +12,10 @@ public class Factorial {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("you want factorial of which number");
+        
+       //input of given number
         int n = in.nextInt();
         int fact = 1;
-
         for(int i = n; 1 <= i; --i) {
             fact *= i;
         }
