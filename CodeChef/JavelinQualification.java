0fc6c922e746adec18837cbe5114379fe7670bc4
aadityasinha-dotcom <aadityasinha009@gmail.com>
2021-10-01 18:55:56 +0530

    changes to Javaline Qualification

--- CodeChef/JavelinQualification.java
+++ CodeChef/JavelinQualification.java
@@ -1,3 +1,27 @@
+/*
+There are N players with IDs from 1 to N, who are participating in the Javelin throw competition which has two rounds. The first is the qualification round, followed by the final round. The qualification round has gotten over, and you are given the longest distance that each of the N players has thrown as A1,A2,…,AN. Now, the selection process for the final round happens in the following two steps:
+
+If the longest throw of a player in the qualification round is greater than or equal to the qualification mark of M cm, they qualify for the final round.
+
+If after step 1, less than X players have qualified for the finals, the remaining spots are filled by players who have thrown the maximum distance among the players who have not qualified yet.
+
+You are given the best throws of the N players in the qualification round A1,A2,…,AN and the integers M and X. Print the list of the players who will qualify for the finals in increasing order of their IDs.
+
+Sample Input 1 
+3
+3 8000 2
+5000 5001 5002
+3 5000 2
+7999 7998 8000
+4 6000 3
+5999 5998 6000 6001
+
+Sample Output 1 
+2 2 3
+3 1 2 3
+3 1 3 4
+*/
+
 import java.util.*;
 
 public class JavelinQualification
