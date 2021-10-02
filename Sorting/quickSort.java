87e0a16d90d1bb38feb23260693a6fef852c384e
sunil joshi <55904928+iamsunil25@users.noreply.github.com>
2021-10-02 17:56:30 +0530

    "adding some comments"

--- Sorting/quickSort.java
+++ Sorting/quickSort.java
@@ -1,18 +1,22 @@
+//Q. how to implement quicksort in java? 
+
+
+
 class QuickSort {
     //main method
     public static void main(String args[]) {
-        int arr[] = { 101, 37, 68, 29, 11, 5 };
+        int arr[] = { 101, 37, 68, 29, 11, 5 }; //takingthe input array 
         System.out.println("before sorting array");
         for (int element : arr) {
-            System.out.print(element + " ");
+            System.out.print(element + " "); // input = 101, 37, 68, 29, 11, 5
         }
         System.out.println();
-        int n = arr.length;
+        int n = arr.length; // 6
         QuickSort ob = new QuickSort();
-        ob.sort(arr, 0, n - 1);
+        ob.sort(arr, 0, n - 1); // here we call the sort method for sorting the array 
 
         System.out.println("after sorting array");
-        printArray(arr);
+        printArray(arr); // after performing sort method now we are going to print the sorted array
     }
 
     // Partition Method
@@ -36,8 +40,8 @@ class QuickSort {
     void sort(int arr[], int lowIndex, int highIndex) {
         if (lowIndex < highIndex) {
             int pi = partition(arr, lowIndex, highIndex);
-            sort(arr, lowIndex, pi - 1);
-            sort(arr, pi + 1, highIndex);
+            sort(arr, lowIndex, pi - 1); //left array of pivot
+            sort(arr, pi + 1, highIndex); //right array of pivot
         }
     }
 // method to print array
@@ -45,7 +49,7 @@ class QuickSort {
         int n = arr.length;
 
         for (int i = 0; i < n; ++i)
-            System.out.print(arr[i] + " ");
+            System.out.print(arr[i] + " "); //  output = 5 11 29 37 68 101 
         System.out.println();
     }
 }
