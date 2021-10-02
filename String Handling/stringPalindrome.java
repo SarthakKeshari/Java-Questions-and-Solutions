567e7875bb06caec44d671834a83d707dbeed360
Satyam Chaurasia <31190480+satyam878@users.noreply.github.com>
2021-10-02 23:13:00 +0530

    Update stringPalindrome.java

--- String Handling/stringPalindrome.java
+++ String Handling/stringPalindrome.java
@@ -7,16 +7,16 @@ public class Main
 		String s=sc.nextLine();
 		int l=s.length();
 		int temp=0;
-		for(int i=0,j=l-1;i<=j;i++,j--)
+		for(int i=0,j=l-1;i<=j;i++,j--) //Traverse string from start and end
 		{
-		    if(s.charAt(i)!=s.charAt(j))
+		    if(s.charAt(i)!=s.charAt(j)) //Check Each Character From start and end
 		    {
-		        System.out.println("Not Palindrome");
+		        System.out.println("Not Palindrome"); //if condition fails it displays not palindrome
 		        temp=1;
 		        break;
 		    }
 		}
-		if(temp==0)
+		if(temp==0) //if abpve condition does not satisfies then print palindrome 
 		    System.out.println("Palindrome");
 	}
 }
