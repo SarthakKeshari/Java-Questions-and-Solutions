a84595027454280d498e870a5ccbd89591bca03b
Niranjana <niranjana687@gmail.com>
2021-10-02 10:38:48 +0530

    Update DeleteNode.java

--- LeetCode/DeleteNode.java
+++ LeetCode/DeleteNode.java
@@ -1,6 +1,10 @@
 // https://leetcode.com/problems/delete-node-in-a-linked-list/
 // O(n) solution 
 
+//Aproach: The question gives access to the node to be deleted instead of the head node. The node to be deleted will not 
+//be a tail node hence we will set the value of the current node to be that of the next node and set the next of the 
+//current node to the next of it's next. Simple and efficient solution, 0ms runtime.
+
 public class DeleteNode {
     public class ListNode {
         int val;
