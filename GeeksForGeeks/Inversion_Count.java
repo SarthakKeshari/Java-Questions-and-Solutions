f329667cdad3bda93d92fdb93a2215e4033cf092
RAshid602 <68329487+RAshid602@users.noreply.github.com>
2021-10-03 17:25:33 +0530

    Update Inversion_Count.java

--- GeeksForGeeks/Inversion_Count.java
+++ GeeksForGeeks/Inversion_Count.java
@@ -1,18 +1,21 @@
 import java.util.Scanner;
 
+
 public class Inversion_Count {
-    public static int[] input() {
+    public static int[] input() {   //This function is sued to take input from the user
         System.out.println("Enter size of array: ");
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int a[] = new int[n];
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {   //This forloop will help to store array 
             a[i] = sc.nextInt();
         }
         return a;
 
     }
-    
+             //This is the main logic of the solution 
+             //The first loop will run till the length of array starting from 0
+             //Second loop will run till length f array starting from (i+1);
         public static void inversecount(int a[]) {
             int count=0;
             for(int i=0;i<a.length;i++){
@@ -25,17 +28,16 @@ public class Inversion_Count {
                 }
             }
             System.out.println("The Answer is: ");
-            System.out.println(count);
+            System.out.println(count);//The final answer is printed using this line
         }
     public static void main(String[] args) {
         
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter number of test cases you want to perform: ");
-        int t = sc.nextInt();
+        int t = sc.nextInt();//"t " stores the number of test cases you want to perform
         for (int p = 0; p < t; p++) {
             int a[] = input();
             inversecount(a);
-          
         }
     }
 }
