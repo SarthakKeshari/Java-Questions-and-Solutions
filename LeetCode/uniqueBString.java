45673cb39e4dd26308be194309132fa7d2289c73
Satyam Chaurasia <31190480+satyam878@users.noreply.github.com>
2021-10-04 16:04:20 +0530

    Update uniqueBString.java

--- LeetCode/uniqueBString.java
+++ LeetCode/uniqueBString.java
@@ -1,3 +1,6 @@
+/* Given an array of strings nums containing n unique binary strings each of length n, 
+return a binary string of length n that does not appear in nums. If there are multiple answers, you may return any of them.
+https://leetcode.com/problems/find-unique-binary-string/*/
 class Solution {
     public String findDifferentBinaryString(String[] nums) {
         
