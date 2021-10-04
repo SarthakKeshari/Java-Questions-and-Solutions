cdaa0bf445ad9e33cf38203b8cf70a37a9a9162e
basic-bhavya <bjain.nd@gmail.com>
2021-10-04 23:55:24 +0530

    made neccessary changes acc. to PR

--- LeetCode/ThreeSum.java
+++ LeetCode/ThreeSum.java
@@ -1,43 +1,54 @@
 // Solution to https://leetcode.com/problems/3sum/
 
-// Apporach: We use 2 pointers in front and back of the given array. 
-// To further increase efficiency we use sorted arrays and move the 
-// pointers to use each element only once
+// Problem statement:
+// Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0
 
-public List<List<Integer>> threeSum(int[] nums) {
-    Arrays.sort(nums);
- 
-    ArrayList<List<Integer>> result = new ArrayList<>();
- 
-    for (int i = 0; i < nums.length; i++) {
-        int j = i + 1;
-        int k = nums.length - 1;
- 
+// Apporach: We use 2 pointers in front and back of the given array.
+// We start with the first element and move the front pointer to the second element.
+// From back of the array we move to the second to last element.
+// Then from second element we move the front pointer to the third element.
+// From second last element we move the back pointer to the third last element and so on.
+// We keep moving the front pointer and the back pointer until we reach the end of the array.
+// The arrays are sorted to skip over duplicates and make the algorithm faster.
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+      Arrays.sort(nums);
+  
+      ArrayList<List<Integer>> res = new ArrayList<>();
+  
+      for (int i = 0; i < nums.length; i++) {
+        int j = i + 1, k = nums.length - 1;
+  
         if (i > 0 && nums[i] == nums[i - 1]) {
-            continue;
+          continue; // skip duplicates
         }
- 
+  
         while (j < k) {
-            if (k < nums.length - 1 && nums[k] == nums[k + 1]) {
-                k--;
-                continue;
-            }
- 
-            if (nums[i] + nums[j] + nums[k] > 0) {
-                k--;
-            } else if (nums[i] + nums[j] + nums[k] < 0) {
-                j++;
-            } else {
-                ArrayList<Integer> l = new ArrayList<>();
-                l.add(nums[i]);
-                l.add(nums[j]);
-                l.add(nums[k]);
-                result.add(l);
-                j++;
-                k--;
-            }
+          if (k < nums.length - 1 && nums[k] == nums[k + 1]) {
+            k--;
+            continue; // skip duplicates
+          }
+  
+          // sum of current triplet
+          if (nums[i] + nums[j] + nums[k] > 0) {
+            k--;
+          } else if (nums[i] + nums[j] + nums[k] < 0) {
+            j++;
+          } else {
+            // add triplet to result
+            ArrayList<Integer> list = new ArrayList<Integer>(
+                Arrays.asList(nums[i], nums[j], nums[k])
+            );
+              
+            res.add(list);
+            j++;
+            k--;
+          }
         }
+      }
+  
+      return res;
     }
- 
-    return result;
-}
+  }
+  
