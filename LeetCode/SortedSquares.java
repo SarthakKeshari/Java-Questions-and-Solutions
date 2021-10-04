95a9783bae5b4d32f7d9d09e89ee05588049cab3
Anova07 <65520755+Anova07@users.noreply.github.com>
2021-10-04 16:45:19 +0530

    Update SortedSquares.java

--- LeetCode/SortedSquares.java
+++ LeetCode/SortedSquares.java
@@ -1,39 +1,36 @@
 
-// Java program to Sort square of the numbers 
+// Java program to Sort the square of the numbers 
 // of the array 
 import java.util.*; 
 import java.io.*; 
-  
 public class sortsq  
 { 
    // Function to sort an square array 
     public static void sortSquares(int arr[]) 
     { 
         int n = arr.length; 
-          
         // First convert each array elements 
         // into its square 
         for (int i = 0 ; i < n ; i++) 
             arr[i] = arr[i] * arr[i]; 
    
-        // Sort an array using "inbuild sort function" 
+        //  using "inbuild sort function" for sorting
         // in Arrays class. 
         Arrays.sort(arr); 
-    } 
-      
-    // Driver program to test above function 
+    }
+  //sample driver program
     public static void main (String[] args) 
     { 
-        int arr[] = { -6 , -3 , -1 , 2 , 4 , 5 }; 
+        int arr[] = { -17 , -12 , -1 , 4 , 8 , 15 }; 
         int n = arr.length; 
       
-        System.out.println("Before sort "); 
+        System.out.println("Original "); 
         for (int i = 0; i < n; i++) 
             System.out.print(arr[i] + " "); 
           
         sortSquares(arr); 
         System.out.println(""); 
-        System.out.println("After Sort "); 
+        System.out.println("Implementing sort"); 
         for (int i = 0 ; i < n ; i++) 
             System.out.print(arr[i] + " "); 
   
