5e76dc04a90d19431cb2fbcee287c530882e464e
Vedansh-Keshari <vedanshkeshari14@gmail.com>
2021-10-05 16:18:33 +0530

    Solution of Outer Diamond

--- Sorting/import java.util.Scanner;.java
+++ Sorting/import java.util.Scanner;.java
@@ -3,20 +3,20 @@ import java.util.Scanner;
 class Array_semi_sort{
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter length of array : ");
+        System.out.println("Enter length of array : ");          // To input the size of the array to be entered
         int l=sc.nextInt();
         int ar[]=new int[l];
-        for(int x=0;x<l;x++){
+        for(int x=0;x<l;x++){                                    // To take input for the number of elements in the parent array
             System.out.println("Enter number at index "+x);
             ar[x]=sc.nextInt();
         }
 
-        int odd[]=new int[l/2];
-        int even[]=new int[l-(l/2)];
-        int i=0;
-        int j=0;
+        int odd[]=new int[l/2];                                   // to store the odd place elements of the parent array
+        int even[]=new int[l-(l/2)];                              // to store the even place elements of the parent array
+        int i=0;                                                  // for itration of even elements
+        int j=0;                                                  // for itration of odd elements
 
-        for(int x=0;x<l;x++){
+        for(int x=0;x<l;x++){                                     // to separate odd and even elements in two different arrays
             if(x%2==0){
                 even[i++]=ar[x];
             }
@@ -30,7 +30,8 @@ class Array_semi_sort{
         odd=reverse(odd);
         i=0;j=0;
 
-        for(int x=0;x<l;x++){
+        for(int x=0;x<l;x++){                                      // to add and replace elements back into parent array
+
             if(x%2==0){
                 ar[x]=even[i++];
             }
@@ -44,7 +45,7 @@ class Array_semi_sort{
         }
 
     }
-    public static int[] ascending(int ar[]){
+    public static int[] ascending(int ar[]){               // to sort elements of the array in ascending order as per the question
         int l=ar.length;
         for(int x=0;x<l;x++){
             for(int y=x;y<l-1;y++){
@@ -58,7 +59,7 @@ class Array_semi_sort{
         return ar;
     }
 
-    public static int[] reverse(int ar[]){
+    public static int[] reverse(int ar[]){                 // to be used to reverse the array 
         int l=ar.length;
         int r[]=new int[l];
         for(int x=0;x<l;x++){
@@ -66,5 +67,5 @@ class Array_semi_sort{
         }
         return r;
     }
-    
+
 }
