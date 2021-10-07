7fbdb886e88a476c2a2d3a256b38051ab7772893
Satyam Chaurasia <31190480+satyam878@users.noreply.github.com>
2021-10-07 22:46:18 +0530

    Update wordCount.java

--- LeetCode/wordCount.java
+++ LeetCode/wordCount.java
@@ -6,15 +6,15 @@ The word can be constructed from letters of sequentially adjacent cells,
 class Solution {
     public boolean exist(char[][] board, String word) {
         
-        int M=board.length, N=board[0].length;
+        int M=board.length, N=board[0].length; //Finding out no of rows and column in matrix
         
         for(int i=0;i<M;i++)
         {
             for(int j=0;j<N;j++)
             {
-                if(board[i][j]==word.charAt(0))
+                if(board[i][j]==word.charAt(0)) //comparing each element of matrix with first letter of word
                 {
-                    if(dfs(board,i,j,word,0)) //applying dfs
+                    if(dfs(board,i,j,word,0)) //applying dfs and checking if the word is present or not
                         return true;
                 }   
             }
@@ -24,20 +24,20 @@ class Solution {
     
     public boolean dfs(char[][] board,int i,int j,String word,int pos)
     {
-        if(pos==word.length())
+        if(pos==word.length()) //if position of word is equal to length of word that means word is present in array
             return true;
         
        int M=board.length, N=board[0].length; 
-            if(i < 0 || i >= M || j < 0 || j >= N || board[i][j]!=word.charAt(pos) || board[i][j]=='#') //comparing the required conditions
+            if(i < 0 || i >= M || j < 0 || j >= N || board[i][j]!=word.charAt(pos) || board[i][j]=='#') //checking the necessary condition to find out whether word exists or not
             return false;
         
         char dup=board[i][j];
         board[i][j]='#';
         
-        boolean ans =  dfs(board,  i + 1, j, word, pos + 1) ||
-                            dfs(board, i - 1, j, word, pos + 1) ||
-                                dfs(board, i, j + 1, word, pos + 1) ||
-                                    dfs(board,  i, j - 1, word, pos + 1);
+        boolean ans =  dfs(board,  i + 1, j, word, pos + 1) ||   //traversing one letter right
+                            dfs(board, i - 1, j, word, pos + 1) ||  ////traversing one letter left
+                                dfs(board, i, j + 1, word, pos + 1) ||  //traversing one letter up
+                                    dfs(board,  i, j - 1, word, pos + 1);  ////traversing one letter down
         
         board[i][j]=dup; //backtracking the string 
             return ans;
