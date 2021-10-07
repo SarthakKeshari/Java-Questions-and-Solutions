b20c86627766120c0ad5f1dedc6dfc630a251c3c
Ravi Mandalia <ravim9231@gmail.com>
2021-10-07 23:28:10 +0530

    added comments

--- Pattern_Printing/Letter_I.java
+++ Pattern_Printing/Letter_I.java
@@ -2,17 +2,18 @@ package pattern_program;
 
 /*
 Write a program to print letter H exactly as shown below -
-	$$$$$##$$$$$
-		 ##     
-     	 ##     
-     	 ##     
-     	 ##     
-     	 ##     
-     	 ##     
-     	 ##     
-     	 ##     
-     	 ##     
-	$$$$$##$$$$$
+
+$$$$$##$$$$$
+	 ##     
+   	 ##     
+   	 ##     
+   	 ##     
+   	 ##     
+   	 ##     
+   	 ##     
+   	 ##     
+   	 ##     
+$$$$$##$$$$$
 */
 
 
@@ -21,36 +22,44 @@ import java.util.Scanner;
 public class Letter_I {
 	
 	// main logic
-	public static void printI(int height)
+	public static void printI(int patternHeight)
 	{   
-	    for (int i = 0; i < height; i++)
+		
+	    for (int i = 0; i < patternHeight; i++)
 	    {
-	        for (int j = 0; j < height; j++)
+	        for (int j = 0; j < patternHeight; j++)
 	        {
-	            if ((i == 0 || i == height - 1) && (j != height-j-1)) {
+				//condition for first and last line of the pattern
+				//also middle of the line
+	            if ((i == 0 || i == patternHeight - 1) && (j != patternHeight-j-1)) {
 	                System.out.printf("$");
-	            } else if (j == height / 2 || (i == 0 || i == height - 1)) {
+	            } else if (j == patternHeight / 2 || (i == 0 || i == patternHeight - 1)) {
 	                System.out.printf("##");
 	            }
 	            else {
+					//spacing for perfect alignment
 	                System.out.printf(" ");
 	            }
 	        }
+			//shifting to new line after filling the current line pattern
 	        System.out.printf("\n");
 	    }
 	}
 	
+	//main method
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("Please specify the height of the alphabet and keep that integer odd.");
 		
-		int height = sc.nextInt();
+		
+		int patternHeight = sc.nextInt();
+		
 		if(height % 2 == 0) {
 			System.out.println("Please specify any odd integer for a better view.");
 		} else {
-			printI(height);
+			printI(patternHeight);
 		}
 		
 		sc.close();
