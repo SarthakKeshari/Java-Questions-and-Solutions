ed67e37783f1d8076adec1c0850b1b583553825e
Paul Wilson <35266836+WilsoP@users.noreply.github.com>
2021-10-08 12:06:46 +0100

    Added the "Letter_R" class to the Pattern_Printing package.

--- Pattern_Printing/Letter_R.java
+++ Pattern_Printing/Letter_R.java
@@ -20,28 +20,35 @@ Expected output =
 public class Letter_R {
 
     public static void main(String[] args) {
+        //prints at the start of each line
         final String HASH = "##";
+        //for upper half of the R
         final String THREE_ATS = "@@@";
+        //for the lower half of the r
         final String TWO_ATS = "@@";
+
 
         var tabs = "   ";
         var spaces = new StringBuilder(" ");
 
+        //Loop through upper half of the R
         for (int i = 1; i < 6; i++) {
             System.out.print(HASH);
-
+            //determines if any indentation is necessary
             if (i % 2 == 0) {
                 System.out.print(tabs);
             } else if (i == 3) {
-                System.out.print(tabs);
-                System.out.print(tabs);
+                System.out.print(tabs+tabs);
             }
+            //prints the ascii for the R
             System.out.print(THREE_ATS + "\n");
         }
 
-        for (int i = 0; i < 4; i++){
+        //loop through lower half of the R
+        for (int i = 0; i < 4; i++) {
             System.out.print(HASH);
             System.out.print(spaces + TWO_ATS + "\n");
+            //increases the space with each iteration of loop
             spaces.append(" ");
         }
     }
