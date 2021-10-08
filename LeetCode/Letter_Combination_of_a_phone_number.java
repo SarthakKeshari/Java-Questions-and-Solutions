b77d1cf787c89b6ccfbc644c95795f7167fe8ea7
Raghav Arora <66276244+Raghavarora27@users.noreply.github.com>
2021-10-08 15:06:19 +0530

    Update Letter combination of  a Phone Number

--- LeetCode/Letter_Combination_of_a_phone_number.java
+++ LeetCode/Letter_Combination_of_a_phone_number.java
@@ -10,6 +10,7 @@
 class Solution {
     public String [] key = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
     public List<String> letterCombinations(String str) {
+        // Edge Case - if string length is equal to zero then we have to return the empty ArrayList
         if(str.length() == 0){
             return new ArrayList<>();
         }
@@ -18,22 +19,24 @@ class Solution {
         return ans;
     }
     public List<String> letterCombinations1(String str) {
+        // Base Case
+        // if String length is zero then we will make a new ArrayList, add empty string to it and return it.
         if(str.length() == 0){
             List<String> base = new ArrayList<>();
             base.add("");
             return base;
         }
         
-        // String = 23
+        // For example if String = 23
         
-        char ch = str.charAt(0); // 2
-        String code = key[ch - '0']; // abc
+        char ch = str.charAt(0); // Here, we are just taking out the 1st character of the string , i.e 2.
+        String code = key[ch - '0']; // Here, we are taking out the String from key Array at index 2, i.e, abc.     
         
         
-        List<String> recAns = letterCombinations1(str.substring(1)); // 3
-        List<String> ans = new ArrayList<>();
+        List<String> recAns = letterCombinations1(str.substring(1)); // Recursive call // sending the remaining substring to the function again
+        List<String> ans = new ArrayList<>();   // Making our own Answer ArrayList
         
-        for(int i=0;i<code.length();i++){
+        for(int i=0;i<code.length();i++){   // In this, we are making the combinations and adding it to our answer arraylist.
             for(String s : recAns)
                 ans.add(code.charAt(i) + s);
         }
