0d7aa181cf4d9a12cea5bd087dd03be85737fa54
nobita851 <54791621+nobita851@users.noreply.github.com>
2021-10-10 05:24:05 +0530

    Update directedGraph.java
    Added requested comments

--- Data Structure/Graph/Directed Graph/directedGraph.java
+++ Data Structure/Graph/Directed Graph/directedGraph.java
@@ -5,9 +5,10 @@ import java.util.*;
 
 public class directedGraph
 {	
-	int size;
-	LinkedList<Integer> adjacencyList[];
+	int size;					// stores number of vertices in graph
+	LinkedList<Integer> adjacencyList[];		// stores the connected vertices with vertex i
 
+	// constructor to initialize instance variables
 	directedGraph(int n)
 	{
 		size = n;
@@ -16,24 +17,35 @@ public class directedGraph
 			adjacencyList[i] = new LinkedList();
 	}
 
+	// method to add edges to the graph
 	void addEdge(int v, int w) 
-    	{ 
+    	{
+		// storing the edge once as it is directed graph
         	adjacencyList[v].add(w); 
     	}
 
+	
+	// method to print the graph
 	void printGraph()
-	{
-		
+	{		
 		System.out.println("\nAdjacency list representation of graph");
+		
+		// printing the vertices connected with vertex i
 		for (int i = 0; i < adjacencyList.length; i++)
 			System.out.println(i+" : "+adjacencyList[i]);
 		
 	}
 
+	// driver code
 	public static void main(String[] args)
 	{	
+		// creating a directed graph of size 9
 		int numberOfVertices = 9;	
+		
+		// creating an instance of directedGraph
 		directedGraph graph = new directedGraph(numberOfVertices);
+		
+		// adding different edges
 		graph.addEdge(0, 3);
 		graph.addEdge(1, 7);
 		graph.addEdge(2, 5);
@@ -46,6 +58,7 @@ public class directedGraph
         	graph.addEdge(8, 6);
         	graph.addEdge(8, 2);
 
+		// calling print method to print the graph
 		graph.printGraph();
 	}
 }
