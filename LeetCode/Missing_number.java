5e40bcc93c6afef6067eee418a472a04effdec50
Rashid <19051001@kiit.ac.in>
2021-10-09 14:17:49 +0530

    testing missing soln

--- LeetCode/Missing_number.java
+++ LeetCode/Missing_number.java
@@ -1,15 +1,12 @@
-//Given an array nums containing n distinct numbers in the range [0, n],
-// return the only number in the range that is missing from the array.
-
 package Array;
 import java.util.Arrays;
 
 public class missing {
     public static void main(String[] args) {
-        int a[]={1,2};  //INput array
-        int n=a.length; //Taking its length in variable n
-        int missing=-1;  //Since the input is betweem [0,n] we set missing -1;
-        Arrays.sort(a);//Sorting the Array
+        int a[]={0,1,2,4};
+        int n=a.length;
+        int missing=-1;
+        Arrays.sort(a);
         for(int i=0;i<n;i++){
             if(i!=a[i]){
                 missing=i;
@@ -23,6 +20,6 @@ public class missing {
         for(int i=0;i<n;i++){
             System.out.println(a[i]);
         }
-        System.out.println(missing);  //This will display the Answer.
+        System.out.println("Missing number is "+missing);
     }
 }
