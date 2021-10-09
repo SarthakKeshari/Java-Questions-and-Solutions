ea3a88b35a2bee91b8fb3658e69c5fca518d6658
Mohit Khedkar <mohitkhedkar521@gmail.com>
2021-10-09 19:20:00 +0530

    Update Letter_B.java

--- Pattern_Printing/Letter_B.java
+++ Pattern_Printing/Letter_B.java
@@ -12,12 +12,14 @@ public class Letter_B {
         {
             for (int j=0;j<=height/2 ;j++ )
             {
+                // for vertical line of letter B
                 if (j==0)
                 {
                     System.out.print("**");
-                }else  if (i==0&&j!=height/2||j==height/2&&i!=0&&i!=height/2&&i!=height-1||i==height/2&&j!=height/2||i==height-1&&j!=height/2) {
+                } // for all the curved strokes of letter B 
+                else  if (i==0&&j!=height/2||j==height/2&&i!=0&&i!=height/2&&i!=height-1||i==height/2&&j!=height/2||i==height-1&&j!=height/2) {
                     System.out.print("##");
-                }
+                } // for empty spaces 
                 else
                 {
                     System.out.print("  ");
