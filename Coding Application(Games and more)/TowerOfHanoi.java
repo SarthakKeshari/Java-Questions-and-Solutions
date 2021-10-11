a8cac59b9c37c326cefe6063f1649ed2ebe4959d
nobita851 <54791621+nobita851@users.noreply.github.com>
2021-10-11 17:44:56 +0530

    Update TowerOfHanoi.java
    Commented the question.

--- Coding Application(Games and more)/TowerOfHanoi.java
+++ Coding Application(Games and more)/TowerOfHanoi.java
@@ -1,5 +1,14 @@
 import java.util.Scanner;
 
+/**
+Tower of Hanoi is a mathematical puzzle where we have three rods and n disks. 
+The objective of the puzzle is to move the entire stack to another rod, obeying the following simple rules:
+	1) Only one disk can be moved at a time.
+	2) Each move consists of taking the upper disk from one of the stacks and placing it on top of another stack 
+	   i.e. a disk can only be moved if it is the uppermost disk on a stack.
+	3) No disk may be placed on top of a smaller disk.
+*/
+
 // Java recursive program to solve tower of hanoi puzzle
 
 public class TowerOfHanoi
