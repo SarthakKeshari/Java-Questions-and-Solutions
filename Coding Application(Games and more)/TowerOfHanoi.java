3302b4dcb402e8bbe8024dd12e5c02446b04e2ad
nobita851 <54791621+nobita851@users.noreply.github.com>
2021-10-11 17:27:00 +0530

    Update TowerOfHanoi.java
    Corrected Indentation

--- Coding Application(Games and more)/TowerOfHanoi.java
+++ Coding Application(Games and more)/TowerOfHanoi.java
@@ -8,46 +8,46 @@ public class TowerOfHanoi
 	void towerOfHanoi(int n, String sourceRod, String targetRod, String auxRod)
 	{
 		// disk = 1, simply move the disk to target rod
-        if (n == 1)
+        	if (n == 1)
 		{
 			System.out.println("Move disk 1 from rod " + sourceRod + " to rod " + targetRod);
 			return;
 		}
 
-        // moving n-1 disks to auxiliary rod
-        // so that nth disk can be moved to target rod
+        	// moving n-1 disks to auxiliary rod
+        	// so that nth disk can be moved to target rod
 		towerOfHanoi(n-1, sourceRod, auxRod, targetRod);
 		
-        // displaying the move to be made
-        System.out.println("Move disk " + n + " from rod " + sourceRod + " to rod " + targetRod);
+        	// displaying the move to be made
+        	System.out.println("Move disk " + n + " from rod " + sourceRod + " to rod " + targetRod);
 
-        // moving n-1 disks to target rod above the nth disk
-        // nth disk is already present at taget rod 
+        	// moving n-1 disks to target rod above the nth disk
+        	// nth disk is already present at taget rod 
 		towerOfHanoi(n-1, auxRod, targetRod, sourceRod);
 	}
 	
 	// Driver method
 	public static void main(String args[])
 	{
-        Scanner sc = new Scanner(System.in);
-        towerOfHanoi obj = new towerOfHanoi();
+        	Scanner sc = new Scanner(System.in);
+        	towerOfHanoi obj = new towerOfHanoi();
         
-        // source/start rod
-        System.out.print("Enter source rod: ");
-        String sourceRod = sc.next();
+        	// source/start rod
+        	System.out.print("Enter source rod: ");
+        	String sourceRod = sc.next();
         
-        // target/destination rod
-        System.out.print("Enter target rod: ");
-        String targetRod = sc.next();
-		
-        // auxiliary rod
-        System.out.print("Enter auxiliary rod: ");
-        String auxRod = sc.next();
+        	// target/destination rod
+        	System.out.print("Enter target rod: ");
+        	String targetRod = sc.next();
+			
+        	// auxiliary rod
+        	System.out.print("Enter auxiliary rod: ");
+        	String auxRod = sc.next();
         
-        // Number of disks
-        System.out.print("Enter number of disks: ");
+        	// Number of disks
+        	System.out.print("Enter number of disks: ");
 		int n = sc.nextInt(); 
 		
-        obj.towerOfHanoi(n, sourceRod, targetRod, auxRod); // 1, 2 and 3 are names of rods
+        	obj.towerOfHanoi(n, sourceRod, targetRod, auxRod);
 	}
 }
