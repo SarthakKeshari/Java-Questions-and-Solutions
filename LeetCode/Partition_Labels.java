26e814d6ed531fd46a8ea421bae04b77043f8ae2
nik <77881638+nik132-eng@users.noreply.github.com>
2021-10-12 09:28:33 +0530

    Update Partition_Labels.java

--- LeetCode/Partition_Labels.java
+++ LeetCode/Partition_Labels.java
@@ -1,3 +1,28 @@
+// 763. Partition Labels
+
+// You are given a string s. We want to partition the string into as many parts as possible so that each letter appears in at most one part.
+
+// Return a list of integers representing the size of these parts.
+
+// Example 1:
+
+// Input: s = "ababcbacadefegdehijhklij"
+// Output: [9,7,8]
+// Explanation:
+// The partition is "ababcbaca", "defegde", "hijhklij".
+// This is a partition so that each letter appears in at most one part.
+// A partition like "ababcbacadefegde", "hijhklij" is incorrect, because it splits s into less parts.
+
+// Example 2:
+
+// Input: s = "eccbbbbdec"
+// Output: [10]
+ 
+// Constraints:
+
+// 1 <= s.length <= 500
+// s consists of lowercase English letters.
+
 import java.util.*;
 
 public class Partition_Labels {
