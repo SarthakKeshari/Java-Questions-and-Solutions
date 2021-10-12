fddc20582aee760759b948249c8d5ea57f8bec6d
Ravneet Singh <32276260+ravneetsinghgaba@users.noreply.github.com>
2021-10-13 00:09:35 +0530

    Update RadixSort.java

--- Sorting/RadixSort.java
+++ Sorting/RadixSort.java
@@ -1,3 +1,5 @@
+/* Write a program to sort an array using Radix Sort Algorithm. */
+
 public class Radix {
     public static void main(String[] args) {
         int[] arr={ 4725, 4586, 1330, 8792, 1594, 5729};
